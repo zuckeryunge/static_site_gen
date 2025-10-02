b0100b9ecf179e2692e87bce49069ac5364096c4
Nico Niermann <mail@niconiermann.de>
2025-10-02 23:58:35 +0200

    added html node

---
+++ src/htmlnode.py
@@ -0,0 +1,18 @@
+class HTMLNode():
+
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+
+    def props_to_html(self):
+        temp = ""
+        if self.props != None:
+            for e in self.props:
+                temp +=" " + e
+        return temp
+        
