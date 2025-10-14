ccf9712ac830d2fdbeea9cb22f3e1101de86df7d
Nico Niermann <mail@niconiermann.de>
2025-10-14 08:17:17 +0200

    splitting markdown blocks now

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -3,8 +3,8 @@ def markdown_to_blocks(markdown):
     split_blocks = markdown.split("\n\n")
     clean_blocks = []
     for block in split_blocks:
-        block.strip()
-        if block != "":
-            clean_blocks.append(block)
+        stripped_block = block.strip()
+        if stripped_block != "":
+            clean_blocks.append(stripped_block)
 
     return clean_blocks
