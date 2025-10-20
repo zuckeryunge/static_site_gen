2e049f7a85b9b8421922bf7e7b5a73be18955c1c
Nico Niermann <mail@niconiermann.de>
2025-10-20 23:52:18 +0200

    adding function for copying from static to public directory

--- src/main.py
+++ src/main.py
@@ -1,8 +1,28 @@
-from textnode import TextNode
+import os
+import shutil
 
-print("hello world")
+
+
+def copy_to_public(public_path, static_path):
+    if not os.path.exists(public_path):
+        os.mkdir(public_path)
+        print(f"created directory {public_path}")
+    
+    # if inhalt drin then delete
+    # log
+    #
+    # list all the files of static_src_path
+    # log how many files are there
+    #
+    # copy each element into public_dst_path
+    # log each cpied file and path and error
+
 
 def main():
-    new_one = TextNode("This is some anchor text", "link", "https://www.boot.dev")
-    print(new_one)
+    path_to_public = "../public"
+    path_to_static = "../static"
+    copy_to_public(path_to_public, path_to_static)
+
+
+
 main()
